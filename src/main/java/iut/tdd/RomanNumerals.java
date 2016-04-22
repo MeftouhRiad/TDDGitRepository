d19d23e8caf09f2d5c91d026c2d7eb0f8858c37c
Meftouh Riad <meftouh.r@gmail.com>
2016-04-22 09:41:26 +0200

    Oui

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -3,12 +3,42 @@ package iut.tdd;
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
 		String tmp = "";
-		if (Integer.parseInt(arabe) < 10) {
-			
-		}
-		if (Integer.parseInt(arabe) < 5) {
-			for (int i = 0; i < Integer.parseInt(arabe); i++) {
-				tmp += "I";
+		int nombre = Integer.parseInt(arabe);
+		while (nombre > 0) {
+			if (nombre/1000 > 0) {
+				for (int i = 0; i < nombre/1000; i++) {
+					tmp += 'M';
+				}
+			}
+			if (nombre / 500 > 0) {
+				for (int i = 0; i < nombre/500; i++) {
+					tmp += 'D';
+				}
+			}
+			if (nombre / 100 > 0) {
+				for (int i = 0; i < nombre/100; i++) {
+					tmp += 'C';
+				}
+			}
+			if (nombre / 50 > 0) {
+				for (int i = 0; i < nombre/50; i++) {
+					tmp += 'L';
+				}
+			}
+			if (nombre / 10 > 0) {
+				for (int i = 0; i < nombre/10; i++) {
+					tmp += 'X';
+				}
+			}
+			if (nombre / 5 > 0) {
+				for (int i = 0; i < nombre/5; i++) {
+					tmp += 'V';
+				}
+			}
+			if (nombre / 1 > 0) {
+				for (int i = 0; i < nombre/1; i++) {
+					tmp += 'I';
+				}
 			}
 		}
 		return tmp;
@@ -23,4 +53,21 @@ public class RomanNumerals {
 		return tmp;
 	}
 
+	/*
+	 
+1 = I
+
+5 = V
+
+10 = X
+
+50 = L
+
+100 = C
+
+500 = D
+
+1000 = M
+
+	 */
 }
