3705158a84a7a05abdb295b23f03b34338459628
Meftouh Riad <meftouh.r@gmail.com>
2016-04-22 09:26:19 +0200

    Ajustement

---
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -0,0 +1,26 @@
+package iut.tdd;
+
+public class RomanNumerals {
+	public String convertToRoman(String arabe) {
+		String tmp = "";
+		if (Integer.parseInt(arabe) < 10) {
+			
+		}
+		if (Integer.parseInt(arabe) < 5) {
+			for (int i = 0; i < Integer.parseInt(arabe); i++) {
+				tmp += "I";
+			}
+		}
+		return tmp;
+	}
+
+	public String convertFromRoman(String roman) {
+		String tmp = "";
+		for (int i = 0; i < roman.length(); i++) {
+			if (roman.charAt(i) == 'I')
+				tmp += '1';
+		}
+		return tmp;
+	}
+
+}
